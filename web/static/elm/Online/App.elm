4ebc14eab54b58f14a7fb35efb64a53625689d97
Mathieu Lajugie <mathieu@caring.com>
2016-07-27 08:44:28 -0700

    WIP channel: allow to join channel

--- web/static/elm/Online/App.elm
+++ web/static/elm/Online/App.elm
@@ -1,18 +1,20 @@
 module Online.App exposing (initialModel, update, subscriptions, view)
 
-import Html exposing (Html, div, text, h2, p)
+import Html exposing (Html, div, text, h2, p, button)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 import Json.Encode as JE
 import Json.Decode as JD exposing ((:=))
 import Phoenix.Socket
+import Phoenix.Channel
 
 
--- import Phoenix.Channel
 -- import Phoenix.Push
 
 
 type alias Model =
     { socket : Phoenix.Socket.Socket Msg
+    , status : String
     , latestMessage : String
     }
 
@@ -32,6 +34,7 @@ initSocket =
 initialModel : Model
 initialModel =
     { socket = initSocket
+    , status = "disconnected"
     , latestMessage = ""
     }
 
@@ -39,6 +42,9 @@ initialModel =
 type Msg
     = NoOp
     | PhoenixMsg (Phoenix.Socket.Msg Msg)
+    | JoinChannel
+    | DidJoinChannel
+    | DidLeaveChannel
     | ReceiveChatMessage JE.Value
 
 
@@ -57,6 +63,27 @@ update msg model =
                 , Cmd.map PhoenixMsg phxCmd
                 )
 
+        JoinChannel ->
+            let
+                channel =
+                    Phoenix.Channel.init "subject:lobby"
+                        |> Phoenix.Channel.withPayload userParams
+                        |> Phoenix.Channel.onJoin (always <| DidJoinChannel)
+                        |> Phoenix.Channel.onClose (always <| DidLeaveChannel)
+
+                ( phxSocket, phxCmd ) =
+                    Phoenix.Socket.join channel model.socket
+            in
+                ( { model | socket = phxSocket }
+                , Cmd.map PhoenixMsg phxCmd
+                )
+
+        DidJoinChannel ->
+            ( { model | status = "connected" }, Cmd.none )
+
+        DidLeaveChannel ->
+            ( { model | status = "disconnected" }, Cmd.none )
+
         ReceiveChatMessage raw ->
             case JD.decodeValue chatMessageDecoder raw of
                 Ok content ->
@@ -66,6 +93,12 @@ update msg model =
 
                 Err error ->
                     ( model, Cmd.none )
+
+
+userParams : JE.Value
+userParams =
+    JE.object
+        [ ( "user_id", JE.string "42" ) ]
 
 
 type alias ChatMessage =
@@ -92,7 +125,12 @@ view model =
         [ h2 []
             [ text "Online.elm" ]
         , p [ class "lead" ]
+            [ text <| "status = " ++ model.status ]
+        , p [ class "lead" ]
             [ text <| "latestMessage = " ++ model.latestMessage ]
         , p []
             [ text "TODO: implement in Elm now \x1F913!" ]
+        , button
+            [ class "btn btn-primary", onClick JoinChannel ]
+            [ text "Join Channel" ]
         ]
