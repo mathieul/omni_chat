580ffe62710fa2be0db18a16e125bf749d496188
Mathieu Lajugie <mathieu@caring.com>
2016-07-27 09:24:07 -0700

    fix broken channel junction

--- web/channels/subject_channel.ex
+++ web/channels/subject_channel.ex
@@ -1,15 +1,13 @@
 defmodule OmniChat.SubjectChannel do
   use OmniChat.Web, :channel
 
-  def join("subject:lobby", _payload, socket) do
-    IO.puts "DEBUG>>> join(subject:lobby)"
+  def join("subject:lobby", payload, socket) do
     send self, :after_join
     {:ok, socket}
   end
 
   def handle_info(:after_join, socket) do
-    IO.puts "DEBUG>>> handle_info(:after_join): broadcast! welcome"
     broadcast! socket, "welcome", %{"user" => "system", "body" => "Bonjour vous 😘"}
-    {:no_reply, socket}
+    {:noreply, socket}
   end
 end
