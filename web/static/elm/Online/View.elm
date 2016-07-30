fff715579491d14b68920e8c87b04406caf8226f
Mathieu Lajugie <mathieu@caring.com>
2016-07-30 15:42:35 -0700

    style discussions

--- web/static/elm/Online/View.elm
+++ web/static/elm/Online/View.elm
@@ -1,7 +1,7 @@
 module Online.View exposing (view)
 
-import Html exposing (Html, div, text, h2, p, dl, dt, dd)
-import Html.Attributes exposing (class)
+import Html exposing (Html, div, text, h3, p, dl, dt, dd, a, ul, li)
+import Html.Attributes exposing (class, href)
 import Dict
 import String
 import Online.Types exposing (..)
@@ -11,7 +11,50 @@ import Online.Model exposing (Model)
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ h2 []
+        [ div
+            [ class "card-columns" ]
+            (List.map discussionCardView model.discussions)
+        ]
+
+
+discussionCardView : Discussion -> Html Msg
+discussionCardView discussion =
+    div [ class "card" ]
+        [ div [ class "card-block" ]
+            [ h3
+                [ class "card-title text-xs-center" ]
+                [ text discussion.subject ]
+            , div [ class "row m-t-2" ]
+                [ div [ class "col-xs-6" ]
+                    [ ul
+                        [ class "list-group" ]
+                        (List.map participantLine discussion.participants)
+                    ]
+                , div [ class "col-xs-6" ]
+                    [ a
+                        [ href "", class "btn btn-success btn-block" ]
+                        [ text "Join" ]
+                    ]
+                ]
+            ]
+        , div [ class "card-footer text-muted" ]
+            [ text discussion.last_activity_at ]
+        ]
+
+
+participantLine : Participant -> Html Msg
+participantLine participant =
+    li [ class "list-group-item" ] [ text participant.nickname ]
+
+
+
+-- Debug
+
+
+viewDebug : Model -> Html Msg
+viewDebug model =
+    div [ class "container" ]
+        [ h3 []
             [ text "Online.elm" ]
         , div []
             (List.map keyValuePair
