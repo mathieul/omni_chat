e234d12216e30f8196203d8cb679bee07d4003af
mathieul <mathieu@caring.com>
2016-08-04 13:39:26 -1000

    simplify last activity using Timex.from_now

--- web/serializers/discussion_serializer.ex
+++ web/serializers/discussion_serializer.ex
@@ -5,17 +5,6 @@ defmodule OmniChat.DiscussionSerializer do
   attributes [:subject, :participants, :last_activity]
 
   def last_activity(discussion, _conn) do
-    last_activity =
-      Timex.now
-      |> Timex.diff(discussion.last_activity_at, :minutes)
-      |> Duration.from_minutes
-      |> Timex.format_duration(:humanized)
-
-    case last_activity do
-      "" ->
-        "just now"
-      time_ago ->
-        "#{time_ago} ago"
-    end
+    Timex.from_now(discussion.last_activity_at)
   end
 end
