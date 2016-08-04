27462621da3732e7ccb95fe887df91e81ecbc2f1
mathieul <mathieu@caring.com>
2016-08-03 16:15:40 -1000

    query participants

--- web/models/discussion.ex
+++ web/models/discussion.ex
@@ -1,6 +1,7 @@
 defmodule OmniChat.Discussion do
   use OmniChat.Web, :model
   alias OmniChat.DiscussionMessage
+  alias OmniChat.Chatter
 
   schema "discussions" do
     field :subject, :string
@@ -15,5 +16,14 @@ defmodule OmniChat.Discussion do
     |> cast(params, [:subject])
     |> validate_required([:subject])
     |> unique_constraint(:subject)
+  end
+
+  def fetch_participant_nicknames(discussion) do
+    query = from dm in DiscussionMessage,
+      join: c in Chatter, on: c.id == dm.chatter_id,
+      where: dm.discussion_id == ^discussion.id,
+      distinct: c.nickname,
+      select: c.nickname
+    Repo.all query
   end
 end
