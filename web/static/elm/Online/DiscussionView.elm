a2b2705e615ec07a1d455ccf704bd6b6a7b54fac
mathieul <mathieu@caring.com>
2016-08-06 17:29:09 -1000

    start laying out discussion page - TODO: scrollIntoView() last message

--- web/static/elm/Online/DiscussionView.elm
+++ web/static/elm/Online/DiscussionView.elm
@@ -1,8 +1,8 @@
 module Online.DiscussionView exposing (view)
 
-import Html exposing (Html, div, text, button)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, text, form, input, button, strong)
+import Html.Attributes exposing (class, type', style, id)
+import Html.Events exposing (onSubmit)
 import Online.Types exposing (..)
 import Online.Model exposing (Model)
 import Online.TopBarView as TopBarView
@@ -15,10 +15,62 @@ view discussion model =
             { title = discussion.subject
             , back = Just ShowDiscussions
             }
-        , div [] [ text <| "DISCUSSION ROUTE ID=" ++ (toString discussion.id) ]
-        , button
-            [ class "btn btn-alternate"
-            , onClick ShowDiscussions
+        , messages model
+        , editor model
+        ]
+
+
+messages : Model -> Html Msg
+messages model =
+    div [ id "discussion-messages" ]
+        [ myMessage "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor"
+        , theirMessage "fifi" "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        , theirMessage "zhannulia" "cupidatat non proident"
+        , myMessage "Esse cillum dolore eu fugiat nulla pariatur."
+        , myMessage "Ok..."
+        , theirMessage "riri" "I don't want to!"
+        , myMessage "I figured"
+        ]
+
+
+myMessage : String -> Html Msg
+myMessage content =
+    div [ class "row" ]
+        [ div [ class "col-xs-10 col-xs-offset-2" ]
+            [ div [ class "alert alert-info pull-xs-right" ]
+                [ text content ]
             ]
-            [ text "Back to all discussions" ]
+        ]
+
+
+theirMessage : String -> String -> Html Msg
+theirMessage nickname content =
+    div [ class "row" ]
+        [ div [ class "col-xs-10" ]
+            [ div [ class "alert alert-warning pull-xs-left" ]
+                [ strong [ style [ ( "margin-right", ".5rem" ) ] ] [ text nickname ]
+                , text content
+                ]
+            ]
+        ]
+
+
+editor : Model -> Html Msg
+editor model =
+    div [ class "navbar navbar-fixed-bottom navbar-light bg-faded" ]
+        [ form [ onSubmit ShowDiscussions ]
+            [ div [ class "row" ]
+                [ div [ class "col-xs-8", style [ ( "padding-right", "0" ) ] ]
+                    [ input
+                        [ type' "text"
+                        , class "form-control"
+                        ]
+                        []
+                    ]
+                , div [ class "col-xs-4" ]
+                    [ button [ class "btn btn-primary btn-block" ]
+                        [ text "Send" ]
+                    ]
+                ]
+            ]
         ]
