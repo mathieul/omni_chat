7b750ad652bfce1dbff1c9c9488ff9adbb14d49e
Mathieu Lajugie <mathieu@caring.com>
2016-08-21 21:00:25 -0700

    really bump version this time

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OmniChat.Mixfile do
   def project do
     [
       app: :omni_chat,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
